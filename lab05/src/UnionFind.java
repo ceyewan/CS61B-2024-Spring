25662d9d919369b68684c65c1e3817346e12f708
ceyewan <1020470197@qq.com>
2024-07-12 15:10:18 +0800

    finish lab05 UnionFind 路径压缩+利用负数表示权重

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,37 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnionFind {
-    // TODO: Instance variables
+    int[] array;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return Math.abs(find(v));
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return array[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v >= array.length || v < 0) {
+            throw new IllegalArgumentException("v must >= 0 and < array.length");
+        }
+        List<Integer> tmp = new ArrayList<Integer>();
+        while (array[v] >= 0) {
+            tmp.add(v);
+            v = array[v];
+        }
+        for (int v1 : tmp) {
+            array[v1] = v;
+        }
+        return v;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -40,7 +53,20 @@ public class UnionFind {
        root to V2's root. Union-ing an item with itself or items that are
        already connected should not change the structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+        assert 0 <= v1 && v1 < array.length;
+        assert 0 <= v2 && v2 < array.length;
+        int r1 = find(v1);
+        int r2 = find(v2);
+        if (r1 == r2) {
+            return;
+        }
+        if (array[r1] < array[r2]) {
+            array[r1] = array[r1] + array[r2];
+            array[r2] = r1;
+        } else {
+            array[r2] = array[r1] + array[r2];
+            array[r1] = r2;
+        }
     }
 
 }
