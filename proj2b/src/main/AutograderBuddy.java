14fb0ecd8a1dbf2fe0561611aef7aec9ee220077
ceyewan <1020470197@qq.com>
2024-07-17 14:20:53 +0800

    finish proj2b: wordnet

--- proj2b/src/main/AutograderBuddy.java
+++ proj2b/src/main/AutograderBuddy.java
@@ -1,14 +1,15 @@
 package main;
 
 import browser.NgordnetQueryHandler;
+import wordnet.wordGraph;
 
 
 public class AutograderBuddy {
-    /** Returns a HyponymHandler */
-    public static NgordnetQueryHandler getHyponymsHandler(
-            String wordFile, String countFile,
-            String synsetFile, String hyponymFile) {
-
-        throw new RuntimeException("Please fill out AutograderBuddy.java!");
+    /**
+     * Returns a HyponymHandler
+     */
+    public static NgordnetQueryHandler getHyponymsHandler(String wordFile, String countFile, String synsetFile, String hyponymFile) {
+        return new HyponymsHandler(new wordGraph(hyponymFile, synsetFile));
+        // throw new RuntimeException("Please fill out AutograderBuddy.java!");
     }
 }
--- proj2b/src/main/HyponymsHandler.java
+++ proj2b/src/main/HyponymsHandler.java
@@ -1,3 +1,23 @@
 package main;
-public class HyponymsHandler {
+
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import wordnet.wordNet;
+
+import java.util.List;
+
+public class HyponymsHandler extends NgordnetQueryHandler {
+    wordNet wg;
+
+    HyponymsHandler(wordNet wg) {
+        this.wg = wg;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+        return wg.findNext(words).toString();
+    }
 }
